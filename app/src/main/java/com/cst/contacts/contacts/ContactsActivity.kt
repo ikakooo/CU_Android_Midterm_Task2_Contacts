5d5cc5f4cc572cade96d7a40d327fc75f7b58ee7
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-11-02 00:20:40 +0400

    view binding

--- app/src/main/java/com/cst/contacts/contacts/ContactsActivity.kt
+++ app/src/main/java/com/cst/contacts/contacts/ContactsActivity.kt
@@ -1,15 +1,34 @@
 package com.cst.contacts.contacts
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.viewbinding.ViewBinding
 import com.cst.contacts.R
+import com.cst.contacts.databinding.ActivityMainBinding
+import com.cst.contacts.databinding.FragmentContactsBinding
 
 
 class ContactsActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupActionBarWithNavController(findNavController(R.id.fragment))
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+        crossinline bindingInflater: (LayoutInflater) -> T
+    ) = lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }
 }
--- app/src/main/java/com/cst/contacts/contacts/ContactsFragment.kt
+++ app/src/main/java/com/cst/contacts/contacts/ContactsFragment.kt
@@ -3,15 +3,33 @@ package com.cst.contacts.contacts
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log.d
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cst.contacts.R
+import com.cst.contacts.databinding.FragmentContactsBinding
 import com.cst.contacts.donottouch.ContactInfo
 import com.cst.contacts.donottouch.mapToContactInfo
 import com.github.tamir7.contacts.Contacts
 
-class ContactsFragment : Fragment(R.layout.fragment_contacts) {
+class ContactsFragment : Fragment() {
+    private lateinit var binding:FragmentContactsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentContactsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,6 +38,19 @@ class ContactsFragment : Fragment(R.layout.fragment_contacts) {
 
     private fun displayContacts(contacts: List<ContactInfo>) {
         /** ==== თქვენი კოდი ==== **/
+    d("sdfdsfdsf",contacts.toString())
+        binding.SearchEditTextID.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                d("sfsdfsdfsd",p0.toString())
+            }
+
+        })
     }
 
     /** ======== ამის ქვევით კოდს არ შეეხოთ ============= **/
@@ -54,4 +85,5 @@ class ContactsFragment : Fragment(R.layout.fragment_contacts) {
         Contacts.getQuery().find().map {
             it.mapToContactInfo()
         }
+
 }
